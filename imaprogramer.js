6b0f0024f2c505ce6eac284b1f2d6878b116663c
GEOPUBLIO21 <69481817+GEOPUBLIO21@users.noreply.github.com>
2022-12-29 19:10:00 -0500

    load picture from api

--- imaprogramer.js
+++ imaprogramer.js
@@ -1,3 +1,13 @@
 const helloworld = () => {
   alert("helloworld");
 };
+
+const getdogpicture = async () => {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+  const data = await response.json();
+
+  const dogpictureframe = document.getElementById("dogpictureframe");
+
+  dogpictureframe.setAttribute("src", data.message);
+};
